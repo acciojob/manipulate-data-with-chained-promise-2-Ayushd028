048680a229359a6d90ec4e4021b85e069b53d18f
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-04-04 15:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+let outputDiv = document.getElementById("output");
+
+new Promise((resolve) => {
+    setTimeout(() => resolve([1, 2, 3, 4]), 3000);
+})
+.then((arr) => {
+    let evens = arr.filter(num => num % 2 === 0);
+    outputDiv.textContent = evens.join(", ");
+    return new Promise((resolve) => setTimeout(() => resolve(evens), 1000));
+})
+.then((evens) => {
+    let doubled = evens.map(num => num * 2);
+    outputDiv.textContent = doubled.join(", ");
+});
